c37bb3a00ee78c00481cf7b88fbc59f60a7800c3
Arthur Fialho <arthursouza.fp@gmail.com>
2026-04-06 15:42:09 -0300

    fix(api): remove duplicated code in Program.cs causing compilation error

--- backend/GarAgil.Api/Program.cs
+++ backend/GarAgil.Api/Program.cs
@@ -37,6 +37,16 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowFrontend", policy =>
+    {
+        policy.WithOrigins("http://localhost:5173")
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
@@ -44,6 +54,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors("AllowFrontend");
 
 app.UseHttpsRedirection();
 
